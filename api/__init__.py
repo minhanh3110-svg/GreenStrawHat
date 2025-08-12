370bfdd08ea2420980852361b316825331c9f28e
minhanh3110-svg <lehasong@gmail.com>
2025-08-12 15:38:57 +0700

    Update __init__.py

--- api/__init__.py
+++ api/__init__.py
@@ -1,6 +1,7 @@
-from .species import bp as species_bp
-from .batches import bp as batches_bp
-
-def register_api(app):
-    app.register_blueprint(species_bp)
-    app.register_blueprint(batches_bp)
+from flask import Flask
+def create_app():
+    app = Flask(__name__)
+    from app.routes import auth, moi_truong
+    app.register_blueprint(auth.bp)
+    app.register_blueprint(moi_truong.bp)
+    return app
